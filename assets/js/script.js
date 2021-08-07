337f87694eeb86fc57fdb6e8e5e226875b090f0a
John Singapuri <john.singapuri@gmail.com>
2021-08-07 10:12:40 -0600

    added functioning dropdown menus

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,14 @@ let foodBtn = document.getElementById('food-btn');
 let closeBtn = document.querySelector('#close-btn');
 let popupModal = document.querySelector('.modal');
 
+function myGenre() {
+    var genreList = document.getElementById("genreList");
+    document.getElementById("selected-genre").value = genreList.options[genreList.selectedIndex].text;
+    }
+    function myFood() {
+      var foodList = document.getElementById("foodList");
+      document.getElementById("selected-food").value = foodList.options[foodList.selectedIndex].text;
+      }
 /* Functionality for submit buttons */
 // Movie submit button takes in genre, release year, and runtime, checks to make sure all exist, then runs makeUrl, otherwise alerts user to fill out info.
 movieBtn.addEventListener('click', (event) => {
